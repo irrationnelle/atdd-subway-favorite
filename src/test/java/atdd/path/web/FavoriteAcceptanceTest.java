7fed2e45a56b198c8232a325e9603863b622c731
Hyeonseok AHN <hyeonseok@pplink.net>
2020-02-20 22:39:03 +0900

    Add FavoriteAcceptanceTest to fail

--- src/test/java/atdd/path/web/FavoriteAcceptanceTest.java
+++ src/test/java/atdd/path/web/FavoriteAcceptanceTest.java
@@ -1,22 +1,45 @@
 package atdd.path.web;
 
 import atdd.path.AbstractAcceptanceTest;
+import atdd.path.domain.Station;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import reactor.core.publisher.Mono;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FavoriteAcceptanceTest extends AbstractAcceptanceTest {
-    public static final String FAVORITE_URI = "/favorites";
+    public static final String FAVORITE_URI = "/favorite";
+    private String tokenInfo;
+
+    private UserHttpTest userHttpTest;
+
+    @BeforeEach()
+    void setUp() {
+        this.userHttpTest = new UserHttpTest(webTestClient);
+
+        String userUri = "/users";
+        String userInputJson = String.format("{\"email\": \"%s\", \"name\": \"%s\", \"password\": \"%s\"}",
+                "boorwonie@email.com", "브라운", "subway");
+        String signInUri = userUri + "/login";
+        String signInJson = String.format("{\"email\": \"%s\", \"password\" : \"%s\"}", "boorwonie@email.com",
+                "subway");
+
+        userHttpTest.createUserSuccess(userUri, userInputJson);
+        tokenInfo = userHttpTest.createAuthorizationTokenSuccess(signInUri, signInJson);
+    }
 
     @DisplayName("지하철역 즐겨찾기 등록")
     @Test
     public void registerFavoriteForStation() {
-        String inputJson = String.format("{\"name\": \"%s\"", "강남역");
+        String inputJson = String.format("{\"name\": \"%s\"}", "강남역");
 
         webTestClient
                 .post()
                 .uri(FAVORITE_URI + "/stations")
+                .header("Authorization", tokenInfo)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(inputJson), String.class)
                 .exchange()
@@ -24,8 +47,9 @@ public class FavoriteAcceptanceTest extends AbstractAcceptanceTest {
                 .isCreated()
                 .expectHeader()
                 .contentType(MediaType.APPLICATION_JSON)
-                .expectHeader()
-                .exists("Location");
+                .expectBody(FavoriteResponseView.class)
+                .consumeWith(result -> {
+                    assertThat(result.getResponseBody()).isNotNull();
+                });
     }
-
 }
